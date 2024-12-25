e87d57487716f8e5c122d422c83ba31ed29fc396
tiberium <jinzhe.oerv@isrc.iscas.ac.cn>
2024-12-25 11:08:00 +0000

    [Kconfig] add basic env for lightrag

---
+++ init_env.sh
@@ -0,0 +1,19 @@
+## chatanywhere
+export CHAT_URL=https://api.chatanywhere.tech/v1
+export CHAT_KEY=sk-CsmWLCY1rmOQUqQAOcxz6mt3Gfk8Bt0mSDRPrf6uMc61f8tG
+export CHAT_LLM_MODEL=gpt-4o-mini
+export CHAT_EMBEDDING_MODEL=text-embedding-3-large
+## openai like
+export _MOD=CHAT
+export _URL=${_MOD}_URL
+export _KEY=${_MOD}_KEY
+export _LLM_MODEL=${_MOD}_LLM_MODEL
+export _EMBEDDING_MODEL=${_MOD}_EMBEDDING_MODEL
+
+export OPENAI_BASE_URL=${!_URL}
+export OPENAI_API_KEY=${!_KEY}
+export LLM_MODEL=${!_LLM_MODEL}
+export EMBEDDING_MODEL=${!_EMBEDDING_MODEL}
+
+## lightrag http server api
+export RAG_DIR=../rag_dir
